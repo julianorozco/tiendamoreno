ced8bfefae3ae598ad60f0278e23bfdf1a4225df
Julian Orozco <orozcojulian23@gmail.com>
2021-06-20 20:55:46 -0300

    Catálogo con MAPS y Promises

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,13 +1,14 @@
 import { ItemCount } from "../ItemCount";
 
-export const CardComponent = ({ title, img, price }) => {
+export const Item = ({ product }) => {
   return (
     <div className="card">
       <div className="card-body">
-        <h4 className="card-title">{title}</h4>
-        <img src={img} alt="" />
+        <h4 className="card-title">{product.title}</h4>
+        <img src={product.pictureUrl} alt="" />
+        <h6>{product.description}</h6>
         <h5 className="d-flex justify-content-center align-items-center">
-          ${price}
+          ${product.price}
         </h5>
         <ItemCount stock="5" initial="1" />
       </div>
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,15 +1,16 @@
-import { CardComponent } from "../CardComponent";
-import img from "../../img/img";
+import { useState, useEffect } from "react";
+import { ItemList } from "../ItemList";
 
 export const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(async () => {
+    const response = await fetch("./json/products.json");
+    const json = await response.json();
+    setProducts(json);
+  }, []);
   return (
     <>
-      <CardComponent title={"Guantes"} img={img.guantes} price={"150"}/>
-      <CardComponent title={"Crema"} img={img.cremas} price={"250"} />
-      <CardComponent title={"Ambo"} img={img.ambo} price={"200"} />
-      <CardComponent title={"Barbijo"} img={img.barbijo} price={"500"} />
-      <CardComponent title={"Apósito"} img={img.aposito} price={"300"} />
-      <CardComponent title={"Frasco"} img={img.frasco} price={"100"} />
+        <ItemList products={products} />
     </>
   );
 };
